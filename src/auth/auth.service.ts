b812fd657b7d8884644a5a543a5bb06b847b7a2e
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2026-01-15 11:00:55 +0300

    add prisma module and add method register

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterRequest } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async register(dto: RegisterRequest) {
+    const { name, email, password } = dto;
+
+    const existUser = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (existUser) {
+      throw new ConflictException('Пользователь с такой почтой уже существует');
+    }
+
+    const user = await this.prismaService.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+
+    return user;
+  }
+}
