e3ad33831a128c8080733b18c37788c1eb93161a
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2026-01-15 13:53:08 +0300

    install class-validator and validate dto

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,5 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
 export class RegisterRequest {
+  @IsString({ message: 'Имя должно быть строкой' })
+  @IsNotEmpty({ message: 'Имя обязательно для заполнения' })
+  @MaxLength(30, { message: 'Имя не должно превышать 30 символов' })
   name: string;
+
+  @IsString({ message: 'Почта должна быть строкой' })
+  @IsNotEmpty({ message: 'Почта обязательна для заполнения' })
+  @IsEmail({}, { message: 'Некорректный формат электронной почты' })
   email: string;
+
+  @IsString({ message: 'Пароль должен быть строкой' })
+  @IsNotEmpty({ message: 'Пароль обязателен для заполнения' })
   password: string;
 }
